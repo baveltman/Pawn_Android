59c7896b19b50dc9bcf40a5fd812acc2f9d41987
baveltman <baveltman@gmail.com>
2015-04-26 18:51:58 -0500

    Drawer opening and closing on menu or logo click in action bar

--- app/src/main/java/com/baveltman/pawn/PawnActivity.java
+++ app/src/main/java/com/baveltman/pawn/PawnActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -20,10 +23,15 @@ public class PawnActivity extends ActionBarActivity {
     private Typeface mBoldTextTypeFace;
     private Typeface mBlackTypeFace;
 
+    //action bar
+    private TextView mLogo;
+    private ImageView mMenuButton;
+
     //drawer
     private static final String[] mDrawerTitles = {"Log out"};
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +56,7 @@ public class PawnActivity extends ActionBarActivity {
         // Set the adapter for the list view
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, mDrawerTitles));
+
     }
 
     private void bindCustomActionBar() {
@@ -57,8 +66,24 @@ public class PawnActivity extends ActionBarActivity {
         LayoutInflater mInflater = LayoutInflater.from(this);
 
         View mCustomView = mInflater.inflate(R.layout.custom_action_bar, null);
-        TextView mTitleTextView = (TextView) mCustomView.findViewById(R.id.title_text);
-        mTitleTextView.setTypeface(getLogoTypeFace());
+        mLogo = (TextView) mCustomView.findViewById(R.id.title_text);
+        mLogo.setTypeface(getLogoTypeFace());
+
+        mLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+
+        mMenuButton = (ImageView)mCustomView.findViewById(R.id.menue_button);
+
+        mMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
 
         mActionBar.setCustomView(mCustomView);
         mActionBar.setDisplayShowCustomEnabled(true);
